d5ce2e8a2b1acb1af93890dca778e2c781b3e891
Erik Timmers <e.timmers@gmail.com>
2016-02-13 22:43:44 +0100

    Add (misplaced, super-pixel precision) points

--- src/component/wr-frame.js
+++ src/component/wr-frame.js
@@ -1,11 +1,15 @@
+// TODO own magnifier -> sub-pixel accuracy
+
 import 'jquery';
 import 'okfocus/okzoom/src/okzoom.min';
 import {bindable, computedFrom} from 'aurelia-framework';
+import {Point} from '../model/point';
 
 export class WrFrame {
   @bindable frame;
   reference;
   activeFrame;
+  activePoints;
 
   @computedFrom('activeFrame')
   get source() {
@@ -18,13 +22,30 @@ export class WrFrame {
       height: 100,
       scaleWidth: 2 * this.frame.width
     });
+    $('.ok-listener').click(event => {
+      this.click(event.originalEvent);
+    });
+  }
+
+  click(event) {
+    if (!(this.activePoints.size < 3)) {
+      return;
+    }
+    let offsetX = event.offsetX;
+    let offsetY = event.offsetY;
+    let img = $('.wr-frame img').get(0);
+    let x = (offsetX / img.width) * this.activeFrame.width;
+    let y = (offsetY / img.height) * this.activeFrame.height;
+    this.activePoints.add(new Point(x, y));
   }
 
   toggle() {
     if (!this.activeFrame || this.activeFrame != this.frame) {
       this.activeFrame = this.frame;
+      this.activePoints = this.frame.points;
     } else {
       this.activeFrame = this.reference;
+      this.activePoints = this.frame.refPoints;
     }
   }
 
@@ -32,5 +53,15 @@ export class WrFrame {
     this.reference = this.frame.reference;
     this.activeFrame = null;
     this.toggle();
+  }
+
+  left(point) {
+    let img = $('.wr-frame img').get(0);
+    return (point.x * (img.width / this.activeFrame.width)).toFixed(0);
+  }
+
+  top(point) {
+    let img = $('.wr-frame img').get(0);
+    return (point.y * (img.height / this.activeFrame.height)).toFixed(0);
   }
 }
